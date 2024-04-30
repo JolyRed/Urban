d638f6ffe2a262b6c3292a2b89b10eb610b170e9
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-04-30 18:23:10 +0300

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -2,8 +2,7 @@ string = 'это моя строка'
 
 print(string[0])
 print(string[-1])
-print(string[2:4])
+print(string[2:5])
 print(string[::-1])
 print(len(string))
 print('это новая строка ' + string)
-
