3634af1cef364cc87a85ebb73f88048109092f50
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-05-02 05:09:06 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,22 @@
+my_list = ['apple', 'banana', 'potato', 'monana', 'buenos']
+
+print('List:', my_list)
+print('First element:', my_list[0], 'Last element:', my_list[-1])
+
+sublist = my_list[2:5]
+
+print('Sublist:', sublist[2:5])
+my_list[2] = 'brue'
+print('Modified list:', my_list)
+
+print("*" * 15)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict['orange'])
+my_dict['kiwi'] = 'киви'
+print('Modified dictionary:', my_dict)
+
+
+
+
