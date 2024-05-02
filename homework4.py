4f3a57763cacfc760a314c646ffd0fbedfea02c8
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-05-02 04:52:57 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+immutable_var = (1, 2, 3, 'a', 'b', 3.5)
+
+print('Immutable tuble:', immutable_var)
+
+mutable_list = [1, 2, 3, 4, 5, 'a', 'b,', 3.5]
+
+mutable_list[0] = 200
+mutable_list[1] = 'bc'
+
+print('Mutable list:', mutable_list)
+
