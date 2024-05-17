d1be2a96dd845e93d19fc1cf1250adaf1a6475e1
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-05-17 20:48:21 +0300

    Add files via upload

---
+++ spacename.py
@@ -0,0 +1,14 @@
+def test():
+    a = 10
+    b = 10
+    print(a)
+    print(b)
+
+
+def test2(a, b, c):
+    print(a, b, c, sep='\n')
+
+
+
+test()
+test2(a = 123, b = 'string', c = [1234, 34566])
