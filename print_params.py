5bf32a0114cd36fef34af125fbc382a9ef31a238
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-05-21 14:31:15 +0300

    Add files via upload

---
+++ print_params.py
@@ -0,0 +1,21 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(a=1, b='строка', c=True, d=[1,2,3,4])
+
+print_params(b=25)
+print_params(c=[1,2,3])
+
+
+values_list = [1, True, 'строка']
+values_dict = {1: 'авпвап', 'строка': 1, True: False}
+
+print_params(*values_list, **values_dict)
+
+
+values_list2 = [1, 'строка']
+
+print_params(*values_list2, 42)
+
