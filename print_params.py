f8358b6bd03128b5dd11b454c1c8ddf4b6bf7a9a
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-05-29 22:24:56 +0300

    Update print_params.py

--- print_params.py
+++ print_params.py
@@ -12,7 +12,8 @@ print_params(c=[1,2,3])
 values_list = [1, True, 'строка']
 values_dict = {1: 'авпвап', 'строка': 1, True: False}
 
-print_params(*values_list, **values_dict)
+print_params(*values_list)
+print_params(**values_dict)
 
 
 values_list2 = [1, 'строка']
