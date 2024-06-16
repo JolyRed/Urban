1a45d0af2952668285d13c32a6002ea62b39415a
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-06-16 21:32:50 +0300

    Add files via upload

---
+++ module5hard.py
@@ -0,0 +1,70 @@
+import time
+
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+class Video:
+    def __init__(self, title, duration, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def log_in(self, login, password):
+        for user in self.users:
+            if user.nickname == login and user.password == password:
+                self.current_user = user
+                return
+        print("Неверный логин или пароль")
+
+    def register(self, nickname, password, age):
+        for user in self.users:
+            if user.nickname == nickname:
+                print("Пользователь {} уже существует".format(nickname))
+                return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.log_in(nickname, password)
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *videos):
+        for video in videos:
+            if video not in self.videos:
+                self.videos.append(video)
+
+    def get_videos(self, search_string):
+        matching_videos = []
+        search_string = search_string.lower()
+        for video in self.videos:
+            if search_string in video.title.lower():
+                matching_videos.append(video.title)
+        return matching_videos
+
+    def watch_video(self, video_title):
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return
+        for video in self.videos:
+            if video.title == video_title:
+                if video.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                else:
+                    video.time_now = 0
+                    while video.time_now < video.duration:
+                        time.sleep(1)
+                        video.time_now += 1
+                        print(video.time_now)
+                    print("Конец видео")
+                return
+        print("Видео не найдено")
