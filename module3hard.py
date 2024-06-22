eada0b61b26c28d5c5106befccbaea6beeb5efd9
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-06-23 00:35:56 +0300

    Create module3hard.py

---
+++ module3hard.py
@@ -0,0 +1,28 @@
+def calculate_structure_sum(data_structure):
+  total_sum = 0
+
+  for item in data_structure:
+    if isinstance(item, (int, float)):
+      total_sum += item
+    elif isinstance(item, str):
+      total_sum += len(item)
+    elif isinstance(item, dict):
+      total_sum += calculate_structure_sum(list(item.values()))  # список значений словаря
+    elif isinstance(item, list):
+      total_sum += calculate_structure_sum(item)
+    elif isinstance(item, tuple):
+      total_sum += calculate_structure_sum(list(item))  # список элементов кортежа
+
+  return total_sum
+
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
