8f7e1dcc29c7b932a385f51bcf88345344020270
JolyRed <60690115+JolyRed@users.noreply.github.com>
2024-07-25 22:37:47 +0300

    Create module_9_5.py

---
+++ module_9_5.py
@@ -0,0 +1,21 @@
+class StepValueError(ValueError):
+    pass
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+        
+    def __next__(self):
+        if (self.step > 0 and self.pointer >= self.stop) or (self.step < 0 and self.pointer <= self.stop):
+            raise StopIteration
+        self.pointer += self.step
+        return self.pointer - self.step
